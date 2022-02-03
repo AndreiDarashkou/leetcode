5de1300be6a2dd2f157d5dd759fe67e876738027
AndreiDarashkou <andrei.darashkou@gmail.com>
2022-02-03 14:51:53 +0300

    Algorithms II the second day

--- src/main/java/algorithms2/FindPeakElement.java
+++ src/main/java/algorithms2/FindPeakElement.java
@@ -5,21 +5,20 @@ package algorithms2;
  */
 public class FindPeakElement {
 
-    //todo rewrite to O(logN)
     public static int findPeakElement(int[] nums) {
-        if (nums.length == 1) {
-            return 0;
-        }
-        if (nums.length == 2) {
-            return nums[0] > nums[1] ? 0 : 1;
-        }
-        for (int i = 2; i < nums.length; i++) {
-            if (nums[i - 2] < nums[i - 1] && nums[i - 1] > nums[i]) {
-                return i - 1;
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int middle = (left + right) / 2;
+            if (nums[middle] < nums[middle + 1]) {
+                left = middle + 1;
+            } else {
+                right = middle;
             }
         }
 
-        return -1;
+        return left;
     }
 
 }
--- src/test/groovy/algorithms2/FindPeakElementSpec.groovy
+++ src/test/groovy/algorithms2/FindPeakElementSpec.groovy
@@ -16,7 +16,7 @@ class FindPeakElementSpec extends Specification {
         [8, 1]                   | 0
         [1, 2, 3, 1]             | 2
         [1, 2, 1]                | 1
-        [6, 5, 4, 3, 2, 5, 8, 5] | 6
+        [3, 5, 4, 3, 2, 5, 8, 5] | 1
         [3, 2, 1]                | 0
     }
 }
