7bc96f4cfc7fbc0f9c6a4c21206230316f05495f
AndreiDarashkou <andrei.darashkou@gmail.com>
2022-02-03 16:26:02 +0300

    Algorithms II the forth day BackspaceStringCompare second variant

--- src/main/java/algorithms2/BackspaceStringCompare.java
+++ src/main/java/algorithms2/BackspaceStringCompare.java
@@ -31,4 +31,38 @@ public class BackspaceStringCompare {
         }
         return result.toString();
     }
+
+    //O(1) memory
+    public static boolean backspaceCompare2(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+
+        int sIdx = next(sChars, sChars.length);
+        int tIdx = next(tChars, tChars.length);
+        while (sIdx >= 0 && tIdx >= 0) {
+            if (sChars[sIdx] != tChars[tIdx]) {
+                return false;
+            }
+            sIdx = next(sChars, sIdx);
+            tIdx = next(tChars, tIdx);
+        }
+        return sIdx == tIdx;
+    }
+
+    private static int next(char[] chars, int index) {
+        int back = 0;
+        index = index - 1;
+
+        while (index >= 0) {
+            if (chars[index] == '#') {
+                back++;
+            } else if (back > 0) {
+                back--;
+            } else {
+                return index;
+            }
+            index--;
+        }
+        return index;
+    }
 }
--- src/test/groovy/algorithms2/BackspaceStringCompareSpec.groovy
+++ src/test/groovy/algorithms2/BackspaceStringCompareSpec.groovy
@@ -6,15 +6,30 @@ import spock.lang.Unroll
 class BackspaceStringCompareSpec extends Specification {
 
     @Unroll
-    def "should compare two strings #s and #t and return #result"() {
+    def "should compare two strings #s and #t and return #result (1)"() {
         expect:
         BackspaceStringCompare.backspaceCompare(s, t) == result
         where:
-        s          | t      | result
-        "a"        | "a"    | true
-        "b#"       | "a#"   | true
-        "c#"       | "a#b#" | true
-        "ac##b"    | "b"    | true
-        "abcd####" | ""     | true
+        s          | t       | result
+        "a"        | "a"     | true
+        "b#"       | "a#"    | true
+        "c#"       | "a#b#"  | true
+        "ac##b"    | "b"     | true
+        "abcd####" | ""      | true
+        "abcd####" | "abc##" | false
+    }
+
+    @Unroll
+    def "should compare two strings #s and #t and return #result (2)"() {
+        expect:
+        BackspaceStringCompare.backspaceCompare2(s, t) == result
+        where:
+        s          | t       | result
+        "a"        | "a"     | true
+        "b#"       | "a#"    | true
+        "c#"       | "a#b#"  | true
+        "ac##b"    | "b"     | true
+        "abcd####" | ""      | true
+        "abcd####" | "abc##" | false
     }
 }
